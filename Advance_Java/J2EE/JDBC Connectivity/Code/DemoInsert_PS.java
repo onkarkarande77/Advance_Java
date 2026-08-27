c312d77a9dc2078913448c0b8f24c6fe63eadead
onkarkarande77 <karandeonkar05@gmail.com>
2026-08-27 15:18:20 +0530

    Update DemoInsert_PS.java

--- Advance_Java/J2EE/JDBC Connectivity/Code/DemoInsert_PS.java
+++ Advance_Java/J2EE/JDBC Connectivity/Code/DemoInsert_PS.java
@@ -5,7 +5,7 @@ import java.sql.PreparedStatement;
 
 import com.mysql.cj.jdbc.MysqlDataSource;
 
-public class DemoInsert_PS {
+public class DemoInsert_PS { 
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
